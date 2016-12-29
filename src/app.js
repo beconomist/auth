ee404c32053640d49e51e2c7abeeaafdb8ddc741
beconomist <beconomist@gmail.com>
2016-12-29 14:23:32 +0800

    remove api key

--- src/app.js
+++ src/app.js
@@ -8,13 +8,7 @@ import LoginForm from './components/LoginForm';
 class App extends Component {
   componentWillMount() {
     // firebase initialization
-    firebase.initializeApp({
-      apiKey: 'AIzaSyB2q9wVdIxcCFa53EX9jLoYUwjIG97VrW4',
-      authDomain: 'auth-2e716.firebaseapp.com',
-      databaseURL: 'https://auth-2e716.firebaseio.com',
-      storageBucket: 'auth-2e716.appspot.com',
-      messagingSenderId: '634792994703'
-    });
+  
   }
   render() {
     return (
